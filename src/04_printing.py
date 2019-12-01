ec8deb2bb09f52eecb2350848e4df176c079aa72
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-01 18:47:50 -0500

    04_printing.py done

--- src/04_printing.py
+++ src/04_printing.py
@@ -7,12 +7,26 @@ method, and by using f-strings.
 x = 10
 y = 2.24552
 z = "I like turtles!"
+a = "My name is Nisa"
 
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("x is %s" % x)
+print("y is %s" % y)
+print("z is %s" % z)
 
 # Use the 'format' string method to print the same thing
+print("x is {}".format(x))
+print("y is {}".format(y))
+print("z is {}".format(z))
+
+print("Look ma: {}".format(a))
+
 
 # Finally, print the same thing using an f-string
+print(F"x is {x}")
+print(F"y is {y}")
+print(F"z is {z}")
 
+print(F"Lookie:  {a}")
