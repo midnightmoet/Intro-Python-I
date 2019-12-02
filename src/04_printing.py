da5cd92b9094f68551eded4e929a6a29aa8a12f4
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-02 15:35:52 -0500

    15_classes.py done woo!

--- src/04_printing.py
+++ src/04_printing.py
@@ -3,15 +3,15 @@ Python provides a number of ways to perform printing. Research
 how to print using the printf operator, the `format` string 
 method, and by using f-strings.
 """
+# Using the printf operator (%), print the following feeding in the values of x,
+# y, and z:
+# x is 10, y is 2.25, z is "I like turtles!"
 
 x = 10
 y = 2.24552
 z = "I like turtles!"
 a = "My name is Nisa"
 
-# Using the printf operator (%), print the following feeding in the values of x,
-# y, and z:
-# x is 10, y is 2.25, z is "I like turtles!"
 print("x is %s" % x)
 print("y is %s" % y)
 print("z is %s" % z)
